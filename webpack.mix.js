23ec81a209e37ba76c43e0c5c67b8e945c7bd63c
MarketplaceKit Team <contact@marketplacekit.com>
2019-07-23 19:56:17 +0100

    Clean up default template

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,9 +14,9 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 */
-//mix.sass('resources/themes/default/sass/app.scss', 'public/themes/default/css');
+mix.sass('resources/themes/default/sass/app.scss', 'public/themes/default/css');
 //mix.sass('resources/themes/boomerang/sass/app.scss', 'public/themes/boomerang/css');
 //mix.sass('resources/themes/novum/sass/app.scss', 'public/themes/novum/css');
 //mix.sass('resources/themes/tabler/sass/app.scss', 'public/themes/tabler/css');
 //mix.sass('resources/themes/urban/sass/app.scss', 'public/themes/urban/css');
-mix.sass('resources/themes/novum-rtl/sass/app.scss', 'public/themes/novum-rtl/css');
+//mix.sass('resources/themes/novum-rtl/sass/app.scss', 'public/themes/novum-rtl/css');
